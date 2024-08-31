ca2f1ddcb4f7f0ef81ad650c3861e408f75eae07
Parker Timmerman <parker.timmerman@materialize.com>
2024-08-30 20:51:26 -0400

    another temp fix

--- sysroot/prune_gcc.sh
+++ sysroot/prune_gcc.sh
@@ -2,9 +2,6 @@
 
 set -eux
 
-LIBSTDCXX_VERSION=$1
-LIBSTDCXX_MAJOR=$2
-
 rm -rf /var/buildlibs/gcc/bin
 rm -rf /var/buildlibs/gcc/usr/bin
 rm -rf /var/buildlibs/gcc/libexec
@@ -12,14 +9,14 @@ rm -rf /var/buildlibs/gcc/share
 
 if [ "$ARCH" == 'x86_64' ]; then
   mv /var/buildlibs/gcc/lib/gcc/${ARCH}-linux /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu
-  mv /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/${LIBSTDCXX_VERSION} /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/${LIBSTDCXX_MAJOR}
-  mv /var/buildlibs/gcc/include/c++/${LIBSTDCXX_VERSION} /var/buildlibs/gcc/include/c++/${LIBSTDCXX_MAJOR}
-  mv /var/buildlibs/gcc/include/c++/${LIBSTDCXX_MAJOR}/${ARCH}-linux /var/buildlibs/gcc/include/c++/${LIBSTDCXX_MAJOR}/${ARCH}-linux-gnu
+  mv /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/11.4.0 /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/11
+  mv /var/buildlibs/gcc/include/c++/11.4.0 /var/buildlibs/gcc/include/c++/11
+  mv /var/buildlibs/gcc/include/c++/11/${ARCH}-linux /var/buildlibs/gcc/include/c++/11/${ARCH}-linux-gnu
 elif [ "$ARCH" == 'aarch64' ]; then
   mv /var/buildlibs/gcc/lib/gcc/${ARCH}-linux /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu
-  mv /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/${LIBSTDCXX_VERSION} /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/${LIBSTDCXX_MAJOR}
-  mv /var/buildlibs/gcc/${ARCH}-linux/include/c++/${LIBSTDCXX_VERSION} /var/buildlibs/gcc/${ARCH}-linux/include/c++/${LIBSTDCXX_MAJOR}
-  mv /var/buildlibs/gcc/${ARCH}-linux/include/c++/${LIBSTDCXX_MAJOR}/${ARCH}-linux /var/buildlibs/gcc/${ARCH}-linux/include/c++/${LIBSTDCXX_MAJOR}/${ARCH}-linux-gnu
+  mv /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/11.4.0 /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/11
+  mv /var/buildlibs/gcc/${ARCH}-linux/include/c++/11.4.0 /var/buildlibs/gcc/${ARCH}-linux/include/c++/11
+  mv /var/buildlibs/gcc/${ARCH}-linux/include/c++/11/${ARCH}-linux /var/buildlibs/gcc/${ARCH}-linux/include/c++/11/${ARCH}-linux-gnu
   mv /var/buildlibs/gcc/${ARCH}-linux/include/c++ /var/buildlibs/gcc/include/c++
   mv /var/buildlibs/gcc/${ARCH}-linux/lib64/* /var/buildlibs/gcc/lib64/
   rm -r /var/buildlibs/gcc/${ARCH}-linux

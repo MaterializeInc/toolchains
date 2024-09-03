dc2b1b2f28e43b87a7590edec73ee0e3c278d52e
Parker Timmerman <parker.timmerman@materialize.com>
2024-09-03 16:41:09 -0400

    package linux sysroot with different version of GCC

--- sysroot/prune_gcc.sh
+++ sysroot/prune_gcc.sh
@@ -9,14 +9,14 @@ rm -rf /var/buildlibs/gcc/share
 
 if [ "$ARCH" == 'x86_64' ]; then
   mv /var/buildlibs/gcc/lib/gcc/${ARCH}-linux /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu
-  mv /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/11.4.0 /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/11
-  mv /var/buildlibs/gcc/include/c++/11.4.0 /var/buildlibs/gcc/include/c++/11
-  mv /var/buildlibs/gcc/include/c++/11/${ARCH}-linux /var/buildlibs/gcc/include/c++/11/${ARCH}-linux-gnu
+  mv /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/10.3.0 /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/10
+  mv /var/buildlibs/gcc/include/c++/10.3.0 /var/buildlibs/gcc/include/c++/10
+  mv /var/buildlibs/gcc/include/c++/10/${ARCH}-linux /var/buildlibs/gcc/include/c++/10/${ARCH}-linux-gnu
 elif [ "$ARCH" == 'aarch64' ]; then
   mv /var/buildlibs/gcc/lib/gcc/${ARCH}-linux /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu
-  mv /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/11.4.0 /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/11
-  mv /var/buildlibs/gcc/${ARCH}-linux/include/c++/11.4.0 /var/buildlibs/gcc/${ARCH}-linux/include/c++/11
-  mv /var/buildlibs/gcc/${ARCH}-linux/include/c++/11/${ARCH}-linux /var/buildlibs/gcc/${ARCH}-linux/include/c++/11/${ARCH}-linux-gnu
+  mv /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/10.3.0 /var/buildlibs/gcc/lib/gcc/${ARCH}-linux-gnu/10
+  mv /var/buildlibs/gcc/${ARCH}-linux/include/c++/10.3.0 /var/buildlibs/gcc/${ARCH}-linux/include/c++/10
+  mv /var/buildlibs/gcc/${ARCH}-linux/include/c++/10/${ARCH}-linux /var/buildlibs/gcc/${ARCH}-linux/include/c++/10/${ARCH}-linux-gnu
   mv /var/buildlibs/gcc/${ARCH}-linux/include/c++ /var/buildlibs/gcc/include/c++
   mv /var/buildlibs/gcc/${ARCH}-linux/lib64/* /var/buildlibs/gcc/lib64/
   rm -r /var/buildlibs/gcc/${ARCH}-linux

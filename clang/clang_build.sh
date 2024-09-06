8abaa61316df15339a776543e3b6de42d462d75b
Parker Timmerman <parker.timmerman@materialize.com>
2024-09-05 23:12:55 -0400

    build clang on Linux in Docker

---
+++ clang/clang_build.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the LICENSE file at the
+# root of this repository, or online at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exo pipefail
+
+bootstrap_clang_version="$1"
+clang_version="$2"
+
+# Platform we're currently building on.
+build_arch="$3"
+
+# CPU target that we want to optimize for.
+target_cpu="$4"
+target_arch="$5"
+
+# Assemble all of our variables.
+libs_dir="$build_arch-linux-gnu"
+c_binary="clang-$bootstrap_clang_version"
+cxx_binary="clang++-$bootstrap_clang_version"
+lld_binary="lld-$bootstrap_clang_version"
+
+target_cpu_arg=""
+if [ -n "$target_cpu" ]; then
+    target_cpu_arg="-mcpu=$target_cpu"
+fi
+
+target_arch=""
+target_arch_arg=""
+if [ -n "$target_arch" ]; then
+    target_arch_arg="-march=$target_arch"
+fi
+
+case $build_arch in
+    "amd64")
+        clang_arch=x86_64
+    ;;
+    "arm64")
+        clang_arch=aarch64
+    ;;
+    *)
+        echo "Programming error, unknown platform"
+        exit 1
+    ;;
+esac
+
+clang_target="$clang_arch-unknown-linux-gnu"
+libs_dir="$clang_arch-linux-gnu"
+clang_major_minor_version=$(echo "$clang_version" | cut -d. -f1-2)
+
+# Configure the build.
+
+cmake -G Ninja -S llvm -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_FLAGS="$target_cpu_arg $target_arch_arg -flto=thin -pthread -fPIC -O3 -DNDEBUG" \
+    -DCMAKE_CXX_FLAGS="$target_cpu_arg $target_arch_arg -flto=thin -pthread -fPIC -O3 -DNDEBUG" \
+    -DCMAKE_INTERPROCEDURAL_OPTIMIZATION="on" \
+    -DLLVM_ENABLE_PROJECTS="clang;lld" \
+    -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt" \
+    -DLLVM_DISTRIBUTION_COMPONENTS="clang-resource-headers" \
+    -DCMAKE_C_COMPILER="$c_binary" \
+    -DCMAKE_CXX_COMPILER="$cxx_binary" \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DLLVM_STATIC_LINK_CXX_STDLIB=ON \
+    -DLLVM_USE_LINKER="$lld_binary" \
+    -DLLVM_ENABLE_LTO=Thin \
+    -DLLVM_ENABLE_PIC=ON \
+    -DLLVM_ENABLE_THREADS=ON \
+    -DLLVM_ENABLE_ZLIB=FORCE_ON \
+    -DLLVM_ENABLE_ZSTD=FORCE_ON \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DLLVM_INCLUDE_UTILS=OFF \
+    -DLLVM_INCLUDE_TESTS=OFF \
+    -DLLVM_INCLUDE_EXAMPLES=OFF \
+    -DLLVM_INCLUDE_BENCHMARKS=OFF \
+    -DLLVM_INCLUDE_DOCS=OFF \
+    -DTerminfo_LIBRARIES=/usr/lib/$libs_dir/libtinfo.a \
+    -DZLIB_LIBRARY=/usr/lib/$libs_dir/libz.a \
+    -Dzstd_LIBRARY=/usr/lib/$libs_dir/libzstd.a
+
+# Actually build Clang and friends.
+
+CC="$c_binary" CXX="$cxx_binary" LD="$lld_binary" cmake --build build --target \
+    clang \
+    lld \
+    llvm-ar \
+    llvm-as \
+    llvm-cov \
+    llvm-dwp \
+    llvm-libtool-darwin \
+    llvm-nm \
+    llvm-objcopy \
+    llvm-objdump \
+    llvm-profdata \
+    llvm-strip \
+    llvm-ranlib \
+    cxx \
+    cxxabi \
+    unwind \
+    builtins
---
+++ clang/clang_package.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the LICENSE file at the
+# root of this repository, or online at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exo pipefail
+
+# Version of clang we built.
+clang_version="$1"
+# Architecture we're building for.
+build_arch="$2"
+
+case $build_arch in
+    "amd64")
+        clang_arch=x86_64
+    ;;
+    "arm64")
+        clang_arch=aarch64
+    ;;
+    *)
+        echo "Programming error, unknown platform"
+        exit 1
+    ;;
+esac
+clang_target="$clang_arch-unknown-linux-gnu"
+
+# Package the toolchain by copying everything listed from the following files:
+#
+#  * bin.txt
+#  * include.txt
+#  * lib.txt
+#
+# Note: There is a header file `__config_site` that gets generated in a platform specific
+# directory but to support cross compiling we move it into the normal `include` dir. This is
+# bit hacky, but I've manually confirmed the file is the same for both architectures.
+
+mkdir package
+
+mv llvm-project/build/include/$clang_target/c++/v1/__config_site llvm-project/build/include/c++/v1/__config_site
+
+# Copy all of the files into the 'package' dir.
+
+for dir in bin include lib; do
+    mkdir package/$dir
+    cat $dir.txt | while read -r val; do 
+        # Strip the 'linux:' prefix if it exists.
+        val=${val#linux:}
+        # Skip anything that starts with a 'mac' prefix.
+        if [[ $val != mac* && -n $val ]]; then
+            eval cp -rP llvm-project/build/$dir/$val package/$dir/
+        fi
+    done
+done
+
+# Compress the whole directory.
+
+cd package
+tar -cf - * | zstd --ultra -22 -o "../linux_$clang_arch.tar.zst"
